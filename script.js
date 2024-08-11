d2adadcec6eee7e1b404921d2829048d71adefc7
SAM <sam@SAMs-MacBook-Air.local>
2024-08-12 03:42:20 +0530

    music working and it says that i wanna be yours😉😘

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
-onload = () => {
-    const c = setTimeout(() => {
+window.onload = () => {
+  // Remove the "not-loaded" class after 1 second
+  const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
-    }, 1000);
-  };
+  }, 1000);
+
+  // Create the iframe element
+  const iframe = document.createElement('iframe');
   
+  // Set the iframe attributes
+  iframe.allow = 'autoplay';
+  iframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/366184949&color=%23ff5500&auto_play=true';
+  iframe.style.display = 'none';  // Hide the iframe
+
+  // Append the iframe to the body
+  document.body.appendChild(iframe);
+};
