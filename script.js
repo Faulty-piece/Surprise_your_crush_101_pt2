9791cbc8276b2030b91acbb0f6c6d5edf1f00cf9
SAM <sam@SAMs-MacBook-Air.local>
2024-08-12 04:27:32 +0530

    song file added

--- script.js
+++ script.js
@@ -6,14 +6,4 @@ window.onload = () => {
       clearTimeout(c);
   }, 1000);
 
-  // Create the iframe element
-  const iframe = document.createElement('iframe');
-  
-  // Set the iframe attributes
-  iframe.allow = 'autoplay';
-  iframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/366184949&client_id=nFddmw3ZibOug7XKUPPyXjYCElJCcGcv&color=%23ff5500&auto_play=true';
-  iframe.style.display = 'none';  // Hide the iframe
-
-  // Append the iframe to the body
-  document.body.appendChild(iframe);
 };
