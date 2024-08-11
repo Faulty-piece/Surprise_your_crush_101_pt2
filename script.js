64b7a0982f7bf1dd88ccab3d96a6f39a6989f530
SAM <sam@SAMs-MacBook-Air.local>
2024-08-12 04:00:59 +0530

    music works

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onload = () => {
   
   // Set the iframe attributes
   iframe.allow = 'autoplay';
-  iframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/366184949&client_id=user-870885163&color=%23ff5500&auto_play=true';
+  iframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/366184949&client_id=nFddmw3ZibOug7XKUPPyXjYCElJCcGcv&color=%23ff5500&auto_play=true';
   iframe.style.display = 'none';  // Hide the iframe
 
   // Append the iframe to the body
